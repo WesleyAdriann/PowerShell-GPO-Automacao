942d08fac4efe26f0dab8b2d892149e25caa83f5
Wesley Souza <wesleyadriann@gmail.com>
2019-04-05 18:32:40 -0300

    Cria funções de insert e select

--- renamedescriptionx.ps1
+++ renamedescriptionx.ps1
@@ -7,6 +7,8 @@ function ConnectBD() {
     $database = 'db_renamepc'
     $connectString = "server=$server;user id=$user;password=$password;database=$database"
 
+    
+
     $bdConnect = New-Object MySql.Data.MySqlClient.MySqlConnection($connectString)
     try {
         $bdConnect.Open()
@@ -26,9 +28,55 @@ function CloseBD($bdConnect) {
     write-Host("Conexao fechada")
 }
 
+function SelectQuery($bdConnect, [string]$query) {
+    $bdCommand = $bdConnect.CreateCommand()
+    $bdCommand.CommandText = $query
+
+    $dataAdapter = MySql.Data.MySqlClient.MySqlDataAdapter($bdCommand)
+    
+    $dataSet = New-Object System.Data.DataSet
+
+
+    
+    $bdCommand.ExecuteScalar()
+
+    # $results = $bdCommand.ExecuteReader()
+
+    # $bdCommand.Dispose()
+    # while ($results.Read()) {
+    #     for($i = 0; $i -lt $reader.FieldCount; $i++) {
+    #         write-Host $reader.GetValue($i).ToString()
+    #     }
+    # }
+}
+
+function InsertQuery($bdConnect, [string]$query) {
+    $bdCommand = $bdCommand.CreateCommand()
+    $bdCommand.CommandText = $query
+
+    $RowsInserted = $bdCommand.ExecuteNonQuery()
+
+    $bdCommand.Dispose()
+
+    if($RowsInserted) {
+        write-Host("Inserido $RowInserted")
+    } else {
+        write-Host("Erro $RowInserted")
+    }
+
+}
+
+function MySqlClient() {
+    [void][system.reflection.Assembly]::LoadFrom('C:\MySql.Data.dll')
+    
+    $Client = New-Object MySql.Data.MySqlClient
+    return 
+}
 
 $bdConnect = ConnectBD
+$query = "insert into tb_response values (`ok`, `itautec`)"
 
-
+InsertQuery $bdConnect $query
+# SelectQuery $bdConnect $query
 
 CloseBD $bdConnect
